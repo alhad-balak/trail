26cbd77f43eec8f29a88f0541eb1dbea1718fab2
Abhinav Kumar Gupta <abhinav.iit19@gail.com>
2022-12-15 21:38:05 +0530

    Comleted!

--- server/config.js
+++ server/config.js
@@ -1,60 +1,60 @@
-const mysql = require("mysql");
-
-const mysql_pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "abhinavDB",
-});
-
-console.log("Database ");
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    mysql_pool.getConnection(function (err, connection) {
-        if (err)
-            throw err;
-        connection.query("SELECT * from yogaform", function (err, result) {
-            if (err)
-                console.log("Error");
-            else {
-                console.log("Database Connected");
-                // res.send(result);
-            }
-        });
-
-    });
-
-});
-module.exports = mysql_pool;
-
 // const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
+// const mysql_pool = mysql.createPool({
+//     connectionLimit: 10,
 //     host: "localhost",
 //     user: "root",
 //     password: "",
 //     database: "abhinavDB",
-//     multipleStatements: true
 // });
 
-//To check if the database is connected.
-// connection.connect(function (err) {
-//     if (err)
-//         console.log("ConnectionError--" + err.sqlMessage);
-//     else
-//         console.log("Database Connected");
+// console.log("Database ");
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     mysql_pool.getConnection(function (err, connection) {
+//         if (err)
+//             throw err;
+//         connection.query("SELECT * from yogaform", function (err, result) {
+//             if (err)
+//                 console.log("Error");
+//             else {
+//                 console.log("Database Connected");
+//                 // res.send(result);
+//             }
+//         });
+
+//     });
+
 // });
+// module.exports = mysql_pool;
 
-//Cheecing if the data can be retrived from the database.
-// connection.query("SELECT * from yogaform", function (err, result, fields) {
-//     if (err)
-//         console.log(err);
-//     else
-//         console.log(result);
-// });
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "abhinavDB"
+});
+
+// //To check if the database is connected.
+connection.connect(function (err) {
+    if (err)
+        console.log("ConnectionError--" + err.sqlMessage);
+    else
+        console.log("Database Connected");
+});
+
+// // Cheecing if the data can be retrived from the database.
+connection.query("SELECT * from yogaform", function (err, result, fields) {
+    if (err)
+        console.log(err);
+    else
+        console.log(result);
+});
 
 
-// module.exports = connection;
+module.exports = connection;
