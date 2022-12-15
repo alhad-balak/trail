2c5de7cc61b0820c01d094ced65526eed55eeab0
Abhinav Kumar Gupta <abhinav.iit19@gail.com>
2022-12-15 22:02:11 +0530

    Error handled

--- server/config.js
+++ server/config.js
@@ -1,62 +1,59 @@
-const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "abhinavDB"
-});
-
-// //To check if the database is connected.
-connection.connect(function (err) {
-    if (err)
-        console.log("ConnectionError--" + err.sqlMessage);
-    else
-        console.log("Database Connected");
-});
-
-// // Cheecing if the data can be retrived from the database.
-// connection.query("SELECT * from yogaform", function (err, result, fields) {
-//     if (err)
-//         console.log(err);
-//     else
-//         console.log(result);
-// });
-
-
-module.exports = connection;
-
-
-
 // const mysql = require("mysql");
 
-// const mysql_pool = mysql.createPool({
-//     connectionLimit: 10,
+// const connection = mysql.createConnection({
+//     port: 3306,
 //     host: "localhost",
 //     user: "root",
 //     password: "",
-//     database: "abhinavDB",
+//     database: "abhinavDB"
 // });
 
-// console.log("Database ");
-// const express = require('express');
-// const app = express();
-
-// app.get('/', (req, res) => {
-//     mysql_pool.getConnection(function (err, connection) {
-//         if (err)
-//             throw err;
-//         connection.query("SELECT * from yogaform", function (err, result) {
-//             if (err)
-//                 console.log("Error");
-//             else {
-//                 console.log("Database Connected");
-//                 // res.send(result);
-//             }
-//         });
-
-//     });
-
+// // //To check if the database is connected.
+// connection.connect(function (err) {
+//     if (err)
+//         console.log("ConnectionError--" + err.sqlMessage);
+//     else
+//         console.log("Database Connected");
 // });
-// module.exports = mysql_pool;
+
+// // // Cheecing if the data can be retrived from the database.
+// // connection.query("SELECT * from yogaform", function (err, result, fields) {
+// //     if (err)
+// //         console.log(err);
+// //     else
+// //         console.log(result);
+// // });
+
+
+// module.exports = connection;
+
+
+
+const mysql = require("mysql");
+
+const mysql_pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "abhinavDB",
+});
+
+console.log("Database ");
+const express = require('express');
+const e = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    mysql_pool.getConnection(function (err, connection) {
+        if (err)
+            res.send("Error occured!");
+        else {
+
+            console.log("Database Connected");
+        }
+
+    });
+
+});
+module.exports = mysql_pool;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,79 @@
+// const express = require('express');
+// const app = express();
+
+// const connection = require("./config.js");
+
+// app.get('/', (req, res) => {
+//     connection.query("SELECT * from yogaform", function (err, result) {
+//         if (err)
+//             res.send(err);
+//         else
+//             res.send("Hey! I am working");
+//         // res.send(result);
+//     });
+// });
+
+// const cors = require("cors");
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// }
+
+// app.use(cors(corsOptions)) // Use this after the variable declaration
+
+// app.use(express.json());
+// app.post("/", (req, res) => {
+
+//     const today = new Date();
+//     const todayDate = (today.getFullYear()) + '-' + (today.getMonth() + 1) + '-' + (today.getDate());
+
+//     const data = { ...req.body, date: todayDate };
+
+//     connection.query('INSERT INTO yogaform SET ?', data, (err, result) => {
+//         if (err) {
+//             // console.log(err);
+//             res.send(err);
+//         }
+//         else {
+//             // res.send(result);
+//             console.log("Data inserted into the table");
+//             res.send("Data inserted");
+//         }
+//     });
+// });
+
+// app.listen(5000, () => {
+//     console.log("Server is running on port 5000");
+// });
+
+
+
 const express = require('express');
 const app = express();
 
-const connection = require("./config.js");
+const mysql_pool = require("./config.js");
 
 app.get('/', (req, res) => {
-    connection.query("SELECT * from yogaform", function (err, result) {
-        if (err)
-            res.send(err);
-        else
+    mysql_pool.getConnection(function (err, connection) {
+        connection.query("SELECT * from yogaform", function (err, result) {
+            if (err)
+                res.send(err);
+            else
+                console.log(result);
             res.send("Hey! I am working");
-        // res.send(result);
+        });
     });
 });
 
-const cors = require("cors");
-const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
+// const cors = require("cors");
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+// app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(express.json());
 app.post("/", (req, res) => {
@@ -29,17 +82,18 @@ app.post("/", (req, res) => {
     const todayDate = (today.getFullYear()) + '-' + (today.getMonth() + 1) + '-' + (today.getDate());
 
     const data = { ...req.body, date: todayDate };
-
-    connection.query('INSERT INTO yogaform SET ?', data, (err, result) => {
-        if (err) {
-            // console.log(err);
-            res.send(err);
-        }
-        else {
-            // res.send(result);
-            console.log("Data inserted into the table");
-            res.send("Data inserted");
-        }
+    mysql_pool.getConnection(function (err, connection) {
+        connection.query('INSERT INTO yogaform SET ?', data, (err, result) => {
+            if (err) {
+                // console.log(err);
+                res.send(err);
+            }
+            else {
+                // res.send(result);
+                console.log("Data inserted into the table");
+                res.send("Data inserted");
+            }
+        });
     });
 });
 
